12b58c776ba18fb8c30bd291f2b9cd518ffbf313
Alexandru Oancea <alx.oancea@gmail.com>
2019-03-04 23:09:11 +0200

    add multiple fragments

--- app/src/main/java/com/example/template_android_bottom_navigation_activity/MainActivity.kt
+++ app/src/main/java/com/example/template_android_bottom_navigation_activity/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.template_android_bottom_navigation_activity
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import com.example.template_android_bottom_navigation_activity.fragments.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,19 +12,19 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                replaceFragment(Fragment01())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                replaceFragment(Fragment02())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                replaceFragment(Fragment03())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications2 -> {
-                message.setText(R.string.title_notifications2)
+                replaceFragment(Fragment04())
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -34,5 +36,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/template_android_bottom_navigation_activity/fragments/Fragment01.kt
@@ -0,0 +1,16 @@
+package com.example.template_android_bottom_navigation_activity.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.template_android_bottom_navigation_activity.R
+
+class Fragment01 : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_01, container, false)
+    }
+}
